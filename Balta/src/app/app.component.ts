6134bafe8b476564ccaf1f632b1a1fb31d5d2f57
Jadson Oliveira <jadsonjavamanc@gmail.com>
2024-01-16 08:04:45 -0300

    balta curso

--- Balta/src/app/app.component.ts
+++ Balta/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Todo } from 'src/models/todo.model';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,16 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'OpenERX';
+  public todos: Todo[] = [];
+
+  constructor() {
+    this.todos.push(new Todo(1, false, 1));
+    this.todos.push(new Todo(2, true, 2));
+    this.todos.push(new Todo(3, true, 3));
+    this.todos.push(new Todo(4, true, 4));
+    this.todos.push(new Todo(5, true, 5));
+    this.todos.push(new Todo(6, false, 6));
+    this.todos.push(new Todo(7, true, 7));
+    this.todos.push(new Todo(8, false, 8));
+  }
 }
---
+++ Balta/src/models/todo.model.ts
@@ -0,0 +1,7 @@
+export class Todo {
+    constructor(
+        public title: number,
+        public done: boolean,
+        public id: number,
+    ) { }
+}
